8cd0e49071361218b982f520f23f5a93d6c3cbfe
Maximilian Pressl <maximilian.pressl@stud.unileoben.ac.at>
2020-10-02 16:36:19 +0200

    player

--- RogueLike/player.h
+++ RogueLike/player.h
@@ -1,9 +1,43 @@
 #pragma once
+#include <string>
+#include <vector>
 
 class Player
 {
 private:
-
+	string name;
+	int level;
+	int exp;
+	int health;
+	int armor;
+	int strength;
+	int dextory;
+	int wisdom;
+	int criticalChance;
+	int criticalDamage;
+	int gold;
+	int agility;
+	vector <int> explist;
 public:
-
+	Player(string input);
+	string getPlayername();
+	void changeLevel(int input);
+	int getHealth();
+	void changeHealth(int input);
+	int getArmor();
+	void changeArmor(int input);
+	int getStrength();
+	void changeStrength(int input);
+	int getDextory();
+	void changeDextory(int input);
+	int getWisdom();
+	void changeWisdom(int input);
+	int getCriticalChance();
+	void changeCriticalChance(int input);
+	int getCriticalDamage();
+	void changeCriticalDamage(int input);
+	int getGold();
+	void changeGold(int input);
+	int getAgility();
+	void changeAgility(int input);
 };
